820b86174fd5ba56229cbc8e2ae35a018f7b9921
xin <xinneirong@gamil.com>
2025-11-29 17:04:38 +0800

    CSS模块化实战：企业级样式污染解决方案

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,8 @@
 // import ipad_pro_image from "../assets/images/store-card-ipad-pro.jpeg"
 import ipad_pro_image from "~img/store-card-ipad-pro.jpeg"
 
+import styles from "./Product.module.css"
+
 function Product() {
     const imgStyle = {
         height: "auto",
@@ -11,9 +13,9 @@ function Product() {
     return <div style={{maxWidth: "28rem", position: "relative"}}>
         <img src={ipad_pro_image} alt="iPad pro" style={imgStyle}/>
 
-        <div className="product-text-container">
-            <div className="product-title">iPad pro</div>
-            <div className="product-detail">产品说明</div>
+        <div className={styles.productTextContainer}>
+            <div className={styles["product-title"]}>iPad pro</div>
+            <div className={styles["product-detail"]}>产品说明</div>
         </div>
     </div>;
 }
