b2f17a90feb947b52aac65e9688bd601e53b9259
xin <xinneirong@gamil.com>
2025-11-29 17:20:29 +0800

    Styled-components 样式化组件炫技：动态主题动画响应式一站式开发

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,22 +2,35 @@
 import ipad_pro_image from "~img/store-card-ipad-pro.jpeg"
 
 import styles from "./Product.module.css"
+import styled from "styled-components";
+
+const StyledProductContainer = styled.div`
+  max-width: 28rem;
+  position: relative;
+  trasition: transform 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(${(props) => props.scale || 1});
+    cursor: pointer;
+  }
+`;
+
 
 function Product() {
     const imgStyle = {
         height: "auto",
         width: "100%",
         borderRadius: "0.5rem",
-    }
-
-    return <div style={{maxWidth: "28rem", position: "relative"}}>
-        <img src={ipad_pro_image} alt="iPad pro" style={imgStyle}/>
-
-        <div className={styles.productTextContainer}>
-            <div className={styles["product-title"]}>iPad pro</div>
-            <div className={styles["product-detail"]}>产品说明</div>
-        </div>
-    </div>;
+    };
+    return (
+        <StyledProductContainer scale={1.05}>  // 可以动态调整
+            <img style={imgStyle} src={ipad_pro_image} alt="iPad Pro" />
+            <div className={styles.productTextContainer}>
+                <div className={styles["product-title"]}>iPad Pro</div>
+                <div className={styles["product-detail"]}>磅礴的薄 RMB 8999 起</div>
+            </div>
+        </StyledProductContainer>
+    );
 }
 
 // 导出供外部使用
