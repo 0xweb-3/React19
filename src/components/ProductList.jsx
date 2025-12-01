6f2c5869b675145bc7192638f5e199b0c86a77b0
xin <xinneirong@gamil.com>
2025-12-01 17:00:25 +0800

    React 组件组合：灵活拼装，优雅重用

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,34 +1,57 @@
 import React from 'react'; // 是最核心的库，主要用于用于定义和构建组件；
 import Product from '@components/Product';
 
+const ReleaseNote = () => (
+    <div>
+        <h1>新产品发布日期：2199年1月1日</h1>
+        <h2>请耐心等待</h2>
+    </div>
+)
+
+const ProductNotFound = () => (
+    <div>
+        <h1>产品未发布</h1>
+        <h2>请耐心等待</h2>
+    </div>
+)
+
+const ListTitle = () => (
+    <div style={{display: "flex", justifyContent: "center"}}>
+        <h1 style={{
+            fontWeight: "800",
+            backgroundImage: "url('src/assets/lines.png')",
+            backgroundPosition: "center",
+        }}>
+            上新品，各个添新意
+        </h1>
+    </div>
+)
+
 
 function ProductList({data}) {
     const isReleased = new Date() <= new Date("2199-01-01");
     if (!isReleased) {
-        return (<div>新产品发布日期：2199-01-01，请耐心等待</div>)
+        return <ReleaseNote/>;
     }
 
-    let content;
-    if (data.length > 0) {
-        content = (
-            <div style={{display: 'grid', justifyItems: 'center', rowGap: "3rem"}}>
-                <div style={{display: 'flex', justifyContent: 'center'}}>
-                    <h1 style={{
-                        fontWeight: "800", backgroundImage: "url('src/assets/lines.png')", backgroundPosition: "center",
-                    }}>上新品，各个添新意</h1>
-                </div>
-                {/*必须使用map*/}
-                {data.map((item) => (<Product {...item} key={item.title}/>))}
-            </div>
-        )
-    } else {
-        content = (
-            <div>没有新品</div>
-        )
+    if (data.length <= 0) {
+        return <ProductNotFound/>;
     }
 
-    // 单根节点原则
-    return content;
+    return (
+        <div
+            style={{
+                display: "grid",
+                justifyItems: "center",
+                rowGap: "3rem",
+            }}
+        >
+            <ListTitle/>
+            {data.map((p) => (
+                <Product {...p} key={p.title}/>
+            ))}
+        </div>
+    );
 }
 
 export default ProductList;
