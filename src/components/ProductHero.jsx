624bb8e98534d24f7f0b1144538244cbf336dce5
by0xin <zhongli.news@gmail.com>
2025-12-08 23:01:49 +0800

    复杂State更新的艺术：Immer 黑魔法

--- src/components/ProductHero.jsx
+++ src/components/ProductHero.jsx
@@ -8,84 +8,80 @@ function ProductHero({product, imageUrl}) {
     // const [selectedMemorySize, setSelectedMemorySize] = useState();
 
     const [cartItem, setCartItem] = useState({
-        productId: product.id,
-        imageSrc: product.image,
-        model: null,
-        color: null,
-        memorySize: null
+        productId: product.id, imageSrc: product.image, model: null, color: null, memorySize: null
     })
 
     // 打开了严格模式，会打印2次
     console.log("ProductHero", selectedModel, selectedColor, selectedMemorySize);
 
-    return (
-        <div
-            className="flex flex-col lg:flex-row-reverse
+    return (<div
+        className="flex flex-col lg:flex-row-reverse
         pt-8 mt-4
         md:pt-28 lg:pt-52
         space-y-4
         text-apple-text-light dark:text-apple-text-dark
     "
-        >
-            <div className="flex-1 flex justify-center items-center">
-                <img src={imageUrl} className="w-[350px] lg:-mt-32 lg:ml-19"/>
+    >
+        <div className="flex-1 flex justify-center items-center">
+            <img src={imageUrl} className="w-[350px] lg:-mt-32 lg:ml-19"/>
+        </div>
+        <div className="flex-1 space-y-6 ml-6 md:ml-24">
+            <div className="text-4xl font-black md:text-6xl">
+                购买 {product.name}
             </div>
-            <div className="flex-1 space-y-6 ml-6 md:ml-24">
-                <div className="text-4xl font-black md:text-6xl">
-                    购买 {product.name}
-                </div>
-                <div className="font-medium md:text-xl">
-                    RMB {Number(product.startingPrice).toLocaleString("en-US")}
-                </div>
-                <div className="flex space-x-3">
-                    <SkuSelect
-                        placeholder={"型号"}
-                        options={product.models.map((model) => model.name)}
-                        // onChange={(value) => {
-                        //     setCartItem({...cartItem, model: value})
-                        // }}
-                        onChange={(value) => {
-                            const newState = produce(cartItem, (draft) => {
-                                // draft代表原始的代理副本
-                                draft.model = value;
-                            })
-                            setCartItem(newState)
-                            console.log(newState)
-                        }}
-                        value={cartItem.model}
-                    />
-                    <SkuSelect placeholder={"颜色"} options={product.colors}
-                               onChange={(value) => {
-                                   setCartItem({...cartItem, color: value})
-                               }}
-                               value={cartItem.color}
-                    />
-                    <SkuSelect
-                        placeholder={"储存容量"}
-                        options={product.memorySizes.map((size) => size.name)}
-                        onChange={(value) => {
-                            setCartItem({...cartItem, memorySize: value})
-                        }}
-                        value={cartItem.memorySize}
-                    />
-                    <button
-                        className="
+            <div className="font-medium md:text-xl">
+                RMB {Number(product.startingPrice).toLocaleString("en-US")}
+            </div>
+            <div className="flex space-x-3">
+                <SkuSelect
+                    placeholder={"型号"}
+                    options={product.models.map((model) => model.name)}
+                    // onChange={(value) => {
+                    //     setCartItem({...cartItem, model: value})
+                    // }}
+                    onChange={(value) => {
+                        const newState = produce(cartItem, (draft) => {
+                            // draft代表原始的代理副本
+                            draft.model = value;
+                        })
+                        setCartItem(newState)
+                        console.log(newState)
+                    }}
+                    value={cartItem.model}
+                />
+                <SkuSelect placeholder={"颜色"} options={product.colors}
+                           onChange={(value) => {
+                               setCartItem(produce(cartItem, (draft) => {
+                                   draft.model = value
+                               }))
+                           }}
+                           value={cartItem.color}
+                />
+                <SkuSelect
+                    placeholder={"储存容量"}
+                    options={product.memorySizes.map((size) => size.name)}
+                    onChange={(value) => {
+                        setCartItem({...cartItem, memorySize: value})
+                    }}
+                    value={cartItem.memorySize}
+                />
+                <button
+                    className="
             border border-apple-blue
             px-5 py-2 bg-transparent
             rounded-md
             hover:bg-apple-blue
             hover:text-apple-gray-100
           "
-                        onClick={() => {
-                            alert("加入购物车" + selectedModel + selectedColor + selectedMemorySize)
-                        }}
-                    >
-                        加入购物车
-                    </button>
-                </div>
+                    onClick={() => {
+                        alert("加入购物车" + selectedModel + selectedColor + selectedMemorySize)
+                    }}
+                >
+                    加入购物车
+                </button>
             </div>
         </div>
-    );
+    </div>);
 }
 
 export default ProductHero;
