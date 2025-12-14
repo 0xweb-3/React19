b804f1394ac4bf38676f5d89e77563824751ac57
by0xin <zhongli.news@gmail.com>
2025-12-14 21:57:50 +0800

    fix

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -5,6 +5,8 @@ const MainLayout = ({header, content, footer}: {
     content?: ReactNode,
     footer?: ReactNode
 }) => {
+
+
     return (<div className="bg-apple-light dark:bg-apple-dark">
         {header ?? <h1>默认标题</h1>}
         {content ?? <p>默认内容</p>}
